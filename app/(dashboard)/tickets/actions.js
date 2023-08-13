cb68a49a8fdf0b59101b3952b7c99a9e92c85bfe
KenKot <kennykotten@gmail.com>
2023-08-13 04:14:26 -0700

    use the hook useTransition for server action

--- app/(dashboard)/tickets/actions.js
+++ app/(dashboard)/tickets/actions.js
@@ -21,6 +21,23 @@ export async function addTicket(formData) {
     user_email: session.user.email,
   });
 
+  if (error) {
+    throw new Error("Could not add the new ticket.");
+  }
+
+  revalidatePath("/tickets");
+  redirect("/tickets");
+}
+
+export async function deleteTicket(id) {
+  const supabase = createServerActionClient({ cookies });
+
+  const { error } = await supabase.from("tickets").delete().eq("id", id);
+
+  if (error) {
+    throw new Error("Could not delete the ticket.");
+  }
+
   revalidatePath("/tickets");
   redirect("/tickets");
 }
